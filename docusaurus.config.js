df86d8f87d713ecf0f95dba81ea003ac7cbe6ea8
Manaway <60310284+Manaway@users.noreply.github.com>
2023-03-31 14:56:53 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const config = {
   title: "KingdomsX",
   tagline: "Dinosaurs are cool",
   url: "https://manaway.github.io",
-  baseUrl: "/",
+  baseUrl: "/kingdoms",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
